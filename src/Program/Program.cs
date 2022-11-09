e205b4d1226ca1d7b4333bdc9f2c47e7823d8cdd
atoya200 <agustintoya2004@gmail.com>
2022-11-09 05:27:48 -0300

    Parte 2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,24 +7,47 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            // José es el más veterano, tiene dos hijos, Lucía y Luca
+            Node n1 = new Node("José", 100);
+            Node n2 = new Node("Lucía", 77);
+            Node n3 = new Node("Luca", 72);
+            // Luca tiene 2 hijos Tom y Mary
+            Node n4 = new Node("Tom", 43);
+            Node n5 = new Node("Mary", 27);
+            // Lucía tiene también dos hijos, Frank y Greta
+            Node n6 = new Node("Greta", 40);
+            Node n7 = new Node("Frank", 37);
+            // De los nietos de José,  Mary tiene tres hijos, Linda, Mat y Jess
+            Node n8 = new Node("Linda", 2);
+            Node n9 = new Node("Mat", 7);
+            Node n10 = new Node("Jessie", 5);
 
             n1.AddChildren(n2);
             n1.AddChildren(n3);
 
-            n2.AddChildren(n4);
-            n2.AddChildren(n5);
+            n2.AddChildren(n6);
+            n2.AddChildren(n7);
 
-            n3.AddChildren(n6);
-            n3.AddChildren(n7);
+            n3.AddChildren(n4);
+            n3.AddChildren(n5);
+
+            n5.AddChildren(n8);
+            n5.AddChildren(n9);
+            n5.AddChildren(n10);
+
 
             // visitar el árbol aquí
+            Visitor visitador = new Visitor1();
+            visitador.Visit(n1);
+            Console.WriteLine(visitador.Content);
+            // Parte 1 del desafio
+            visitador = new Visitor2();
+            visitador.Visit(n1);
+            Console.WriteLine(visitador.Content);
+            // Parte 2 del desafio
+            visitador = new Visitor3();
+            visitador.Visit(n1);
+            Console.WriteLine(visitador.Content);
         }
     }
 }
