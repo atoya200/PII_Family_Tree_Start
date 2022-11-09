ccf53e34941bc62976c5c1ce3746e0aeb7ed5aa5
atoya200 <agustintoya2004@gmail.com>
2022-11-09 05:28:16 -0300

    Parte 3

---
+++ src/Library/Visitor.cs
@@ -0,0 +1,22 @@
+using System.Text;
+
+namespace Library
+{
+    public abstract class Visitor
+    {
+        public string Content
+        {
+            get
+            {
+                return this.ContentBuilder.ToString();
+            }
+        }
+
+        protected StringBuilder ContentBuilder { get; } = new StringBuilder();
+
+        public abstract void Visit(Node nodo);
+        public abstract void Visit(Persona persona);
+
+        
+    }
+}
